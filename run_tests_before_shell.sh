bc03e1339d05cbb48932f78e46a740129cd9c63d
Sosuke Kobayashi <sosk@preferred.jp>
2018-05-10 10:02:48 +0900

    del disabled files

--- run_tests_before_shell.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Verify environment by running tests
-# before dropping into a shell.
-set -e
-PYTHONDONTWRITEBYTECODE=1 python -m unittest discover tests/
-echo "Tests passed!"
-
-/bin/bash
